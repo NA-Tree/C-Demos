a46dbd7282389252b7a868dfa4a01065ea4751a2
StormsOE <81591730+NA-Tree@users.noreply.github.com>
2024-01-12 06:32:37 -0700

    added spacing

--- BitwiseOutputs/BitwiseOutputs.c
+++ BitwiseOutputs/BitwiseOutputs.c
@@ -3,13 +3,12 @@
 
 void main(void)
 {
-
 short Bit;
 
-for(int i=0; i<10; i++)
-{
-    Bit = (i & 2);                  //here i is 0000 0001 -> 0000 0010 -> 0000 0011 and so on and you are comparing the 0010 place for the boolean return
-    printf("%d\n", Bit);
-}
+    for(int i=0; i<10; i++)
+    {
+        Bit = (i & 2);                  //here i is 0000 0001 -> 0000 0010 -> 0000 0011 and so on and you are comparing the 0010 place for the boolean return
+        printf("%d\n", Bit);
+    }
 
 }
